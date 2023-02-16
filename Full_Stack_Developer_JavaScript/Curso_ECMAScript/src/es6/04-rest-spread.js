988fd14d7a28ac3a7257be9851fce1dfbe0f39ad
rivaldo <leo_1998@live.cl>
2023-02-15 22:44:22 -0300

    rest spread

---
+++ Full_Stack_Developer_JavaScript/Curso_ECMAScript/src/es6/04-rest-spread.js
@@ -0,0 +1,10 @@
+let fruits = ['apple','banana'];
+let [a,b] = fruits
+console.log(a,fruits[1])
+
+let user = {
+    nameUser:' jose',
+    age: 23
+}
+let {nameUser,age} = user
+console.log(nameUser, user.age)
